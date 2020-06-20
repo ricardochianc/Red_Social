745d41b3ab823f61eb3b6e5d8b2eb01b13024c2f
Ricardo Chian <ricardochianc@gmail.com>
2020-06-20 13:14:04 -0600

    Se agregó función para mostar perfi y menú de acciones

--- social.py
+++ social.py
@@ -32,3 +32,30 @@ def obtener_pais():
     pais = input("Indica tu país de nacimiento: ")
     return pais
 
+def obtener_amigos():
+    linea = input("Escrib una lista con los nombres de tus amigos, separalos con coma ',':")
+    lista_amigos = linea.split(',')
+    return lista_amigos
+
+def mostrar_perfil(nombre, edad, estatura, genero, pais, amigos):
+    
+    print("" * 25, end="\n")
+    print("Nombre:    ", nombre)
+    print("Edad:      ", edad, "años")
+    print("Estatura:  ", estatura, "m")
+    print("Pais:      ", pais)
+    print("Amigos:    ", len(amigos))
+    print("" * 25, end="\n")
+
+def opcion_menu():
+    print("Menú:", "-"*15)
+    print("  1. Escribir un mensaje")
+    print("  2. Mostrar mi muro")
+    print("  3. Mostrar los datos de perfil")
+    print("  4. Actualizar el perfil de usuario")
+    print("  0. Salir")
+    opcion = int(input("Ingresa una opción: "))
+    while opcion < 0 or opcion > 5:
+        print("Inténtalo otra vez.")        
+        opcion = int(input("Ingresa una opción: "))
+    return opcion
